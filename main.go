bb1bc047159f2c96b4e1b395adb3fac9cdfbf1df
SirGFM <gfmandaji@hotmail.com>
2018-02-18 17:36:56 -0300

    Better document main; Add command-line arguments

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "flag"
     "fmt"
     "github.com/SirGFM/GoWebSocketProxy/websocket"
     "github.com/SirGFM/GoWebSocketProxy/proxy"
@@ -16,14 +17,26 @@ func quitCleanup(c chan os.Signal, ctx *websocket.Context) {
 
 func main() {
     var signalTrap chan os.Signal
+    var url string
+    var uri string
+    var port int
 
-    ctx := websocket.NewContext("", "/proxy", 60000, 2)
+    // Parse command line arguments
+    flag.StringVar(&url, "url", "", "URL accepted by the server. Empty defaults to any address/url.")
+    flag.StringVar(&uri, "uri", "/proxy", "URI that references the WebSocket server.")
+    flag.IntVar(&port, "port", 60000, "Port for the WebSocket server.")
+    flag.Parse()
+
+    // Create the WebSocket context
+    ctx := websocket.NewContext(url, uri, port, 2)
     defer ctx.Close()
 
+    // Detect keyboard interrupts (Ctrl+C) and exit gracefully.
     signalTrap = make(chan os.Signal, 1)
     go quitCleanup(signalTrap, ctx)
     signal.Notify(signalTrap, os.Interrupt)
 
+    // Start the server
     err := ctx.Setup(&proxy.Server{})
     if err != nil {
         panic(err.Error())
